c4308f418a6f5bd893d19ffd3c5be90634864d82
viky muhamad <vikymuhamad@gmail.com>
2020-11-25 17:08:52 +0700

    fix

--- frontend/src/resources/screens/OrderScreen.js
+++ frontend/src/resources/screens/OrderScreen.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import { PayPalButton } from 'react-paypal-button-v2'
 import { useSelector, useDispatch } from 'react-redux';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Card, ListGroup } from 'react-bootstrap';
 import MessageBoxComponent from '../components/MessageBoxComponent';
-import { createOrder, getOrderDetails } from "../actions/OrderActions";
+import { getOrderDetails, updateOrderToPay } from "../actions/OrderActions";
 import LoadingBoxComponent from "../components/LoadingBoxComponent";
 import {ORDER_PAY_RESET} from "../constants/OrderConstants";
 import ShippingOrderPartial from "./partials/order/ShippingOrderPartial";
@@ -12,7 +13,7 @@ import OrderItemPartial from "./partials/order/OrderItemPartial";
 const OrderScreen = ({ match, history }) => {
     const dispatch = useDispatch();
 
-    const [ setSdkReady ] = useState(false)
+    const [ sdkReady, setSdkReady ] = useState(false)
 
     const orderId = match.params.id
 
@@ -20,7 +21,7 @@ const OrderScreen = ({ match, history }) => {
     const { order, loading, error } = orderDetails ;
 
     const orderToPay = useSelector((state) => state.orderToPay);
-    const { success: successPay } = orderToPay ;
+    const { loading: loadingPay, success: successPay } = orderToPay ;
 
     const userLogin = useSelector((state) => state.userLogin)
     const { userInfo } = userLogin
@@ -29,6 +30,7 @@ const OrderScreen = ({ match, history }) => {
         const addDecimal = (num) => {
             return (Math.round(num * 100) / 100).toFixed(2)
         }
+
         order.itemsPrice = addDecimal(
             order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
         )
@@ -63,6 +65,12 @@ const OrderScreen = ({ match, history }) => {
         }
     }, [dispatch, orderId, successPay, order])
 
+
+    const successPaymentHandler = (paymentResult) => {
+        console.log(paymentResult)
+        dispatch(updateOrderToPay(orderId, paymentResult))
+    }
+
     return (
        loading ? <LoadingBoxComponent /> : error ?
            <MessageBoxComponent variant='danger'>{error}</MessageBoxComponent> :
@@ -71,10 +79,56 @@ const OrderScreen = ({ match, history }) => {
                <Row>
                    <Col md={8}>
                       <ShippingOrderPartial />
+
                       <OrderItemPartial />
+
                    </Col>
                    <Col md={4}>
-                       <OrderItemPartial />
+                       <Card>
+                           <ListGroup variant='flush'>
+                               <ListGroup.Item>
+                                   <h2>Order Summary</h2>
+                               </ListGroup.Item>
+                               <ListGroup.Item>
+                                   <Row>
+                                       <Col>Items</Col>
+                                       <Col>${order.itemsPrice}</Col>
+                                   </Row>
+                               </ListGroup.Item>
+                               <ListGroup.Item>
+                                   <Row>
+                                       <Col>Discount</Col>
+                                       <Col>${order.discountPrice}</Col>
+                                   </Row>
+                               </ListGroup.Item>
+                               <ListGroup.Item>
+                                   <Row>
+                                       <Col>Shipping</Col>
+                                       <Col>${order.shippingPrice}</Col>
+                                   </Row>
+                               </ListGroup.Item>
+                               <ListGroup.Item>
+                                   <Row>
+                                       <Col>Tax</Col>
+                                       <Col>${order.taxPrice}</Col>
+                                   </Row>
+                               </ListGroup.Item>
+                               <ListGroup.Item>
+                                   <Row>
+                                       <Col>Total</Col>
+                                       <Col>${order.totalPrice}</Col>
+                                   </Row>
+                               </ListGroup.Item>
+                               {!order.isPaid && (
+                                   <ListGroup.Item>
+                                       {loadingPay && <LoadingBoxComponent />}
+                                       {!sdkReady ?  <LoadingBoxComponent /> : (
+                                           <PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler} />
+                                       )}
+                                   </ListGroup.Item>
+                               )}
+                           </ListGroup>
+                       </Card>
                    </Col>
                </Row>
            </>
--- frontend/src/resources/screens/partials/order/OrderItemPartial.js
+++ frontend/src/resources/screens/partials/order/OrderItemPartial.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {Col, Image, ListGroup, Row} from "react-bootstrap";
 import { useSelector } from 'react-redux';
 import MessageBoxComponent from "../../../components/MessageBoxComponent";
